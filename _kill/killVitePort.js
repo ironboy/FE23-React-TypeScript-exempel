357fef52085d77769eb27d32b70bfbc02e76d463
Ironboy <ironboy@users.noreply.github.com>
2024-08-20 23:25:45 +0200

    better port kill on restart

---
+++ _kill/killVitePort.js
@@ -0,0 +1,2 @@
+import { killPortProcess } from "kill-port-process";
+await killPortProcess(5173); // Vite Dev Server
--- kill-ports-on-restart.js
+++
@@ -1,4 +0,0 @@
-import { killPortProcess } from "kill-port-process";
-
-await killPortProcess(5001); // Node.js backend
-await killPortProcess(5173); // Vite Dev Server
