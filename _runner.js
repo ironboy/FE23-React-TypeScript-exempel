14622c91a6a55dd7001416e58fda61f749b1152c
Ironboy <ironboy@users.noreply.github.com>
2024-08-21 06:19:33 +0200

    tested on but mac and win port listening etc in runner

--- _runner.js
+++ _runner.js
@@ -6,6 +6,7 @@ import { spawn } from 'child_process';
 import nodemon from 'nodemon';
 import express from 'express';
 import { killPortProcess } from "kill-port-process";
+import { portToPid } from 'pid-port';
 const sleep = ms => new Promise(res => setTimeout(res, ms));
 
 await killOldMe();
@@ -14,8 +15,13 @@ await startBackend();
 await sleep(2000);
 startVite();
 
+async function killer(port) {
+  await portToPid(port).catch(e => { }) &&
+    killPortProcess(port);
+}
+
 async function startBackend() {
-  spawn('npm', ['run', 'tsc-watch'], { stdio: 'inherit' });
+  spawn('npm run tsc-watch', { shell: true, stdio: 'inherit' });
   while (!fs.existsSync('backendDist')) { await sleep(100); }
   nodemon({
     script: 'backendDist/index.js',
@@ -25,14 +31,14 @@ async function startBackend() {
 }
 
 function startVite() {
-  spawn('npm', ['run', 'dev'], { stdio: 'inherit' });
+  spawn('npm run dev', { shell: true, stdio: 'inherit' });
 }
 
 async function killOldMe() {
-  killPortProcess(5001);
-  killPortProcess(5173);
+  await killer(5001);
+  await killer(5173);
   await fetch('http://127.0.0.1:5002').catch(e => { });
-  killPortProcess(5002);
+  await killer(5002);
 }
 
 // now i listen to a port and can be easily killed
